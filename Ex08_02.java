446ba1d302119e9c76242360b2b3a432a14b1030
Geon Yuk <89954838+Geon27@users.noreply.github.com>
2021-12-05 11:10:43 +0900

    Add files via upload

---
+++ Ex08_02.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Ex08_02 {
+	public static void main(String[] args) {
+		Scanner s = new Scanner(System.in);
+		int[] aa = new int[4];
+		int hap;
+		
+		System.out.printf("1번쨰 숫자를 입력하세요 : ");
+		aa[0] = s.nextInt();
+		System.out.printf("2번쨰 숫자를 입력하세요 : ");
+		aa[1] = s.nextInt();
+		System.out.printf("3번쨰 숫자를 입력하세요 : ");
+		aa[2] = s.nextInt();
+		System.out.printf("4번쨰 숫자를 입력하세요 : ");
+		aa[3] = s.nextInt();
+		
+		hap = aa[0] + aa[1] + aa[2] + aa[3];
+		
+		System.out.printf("합계 ==> %d \n", hap);
+	}
+}
