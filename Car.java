575f17cbb0dad746f424f82b3cf919a7a5577619
Geon Yuk <89954838+Geon27@users.noreply.github.com>
2022-01-18 12:06:00 +0900

    Add files via upload

--- Car.java
+++ Car.java
@@ -1,9 +1,9 @@
-package pack1;
+package pack2;
 
 public class Car {
-	public static String CAR_NAME = "패키지1의 자동차";
+	public static String CAR_NAME = "패키지2의 자동차";
 	
 	public Car() {
-		System.out.println("패키지1의 Car 생성자");
+		System.out.println("패키지2의 Car 생성자");
 	}
 }
