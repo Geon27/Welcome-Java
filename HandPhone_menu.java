b6f0e85c6a5133b684e1b37a39ef5f0b9c1c5573
Geon Yuk <89954838+Geon27@users.noreply.github.com>
2022-01-24 08:19:54 +0900

    Add files via upload

---
+++ HandPhone_menu.java
@@ -0,0 +1,12 @@
+
+public class HandPhone_menu {
+	public static void main(String[] args) {
+		static void print_menu() {
+			System.out.printf("\n");
+			System.out.printf("1. 연락처 출력 \n");
+			System.out.printf("2. 연락처 등록 \n");
+			System.out.printf("3. 연락처 삭제 \n");
+			System.out.printf("4. 끝내기 \n");
+		}
+	}
+}
