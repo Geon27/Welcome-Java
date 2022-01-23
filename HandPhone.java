c81e9f48613203276afff14776f8607a5c21a27e
Geon Yuk <89954838+Geon27@users.noreply.github.com>
2022-01-23 16:11:38 +0900

    Add files via upload

---
+++ HandPhone.java
@@ -0,0 +1,31 @@
+public class HandPhone {
+	static String fname = "c:\\Temp\\juso.txt";
+
+	static class address {
+		String name;
+		String age;
+		String phone;
+		
+		address(String s1, String s2, String s3){
+			this.name = s1;
+			this.age = s2;
+			this.phone = s3;
+		}
+	}
+	
+	public static void main(String[] args) {
+	}
+	
+	static void print_menu() {
+	}
+	
+	static void view_juso() {
+	}
+	
+	static void add_juso() {
+	}
+	
+	static void delete_juso() {
+	}
+	
+}
